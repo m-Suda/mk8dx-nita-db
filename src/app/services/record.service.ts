ad4edda544a0bd7c3479e9bbc8ab1edf115f67c8
masaks <oicwam.orz1228@gmail.com>
2023-01-21 16:47:24 +0900

    record.serviceからstaticなメソッドを切り出した

--- src/app/services/record.service.ts
+++ src/app/services/record.service.ts
@@ -4,12 +4,9 @@ import { combineLatest, from, lastValueFrom, map, tap } from 'rxjs';
 import { environment } from '../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { WorldRecord } from '../types/world-record';
-
-type TaData = {
-    mm: number,
-    ss: number,
-    sss: number
-};
+import { OrLessFilter } from '../types/or-less-filter';
+import { OrLessUtil } from '../classes/or-less-util';
+import { OrLessFilterUtil } from '../classes/or-less-filter-util';
 
 @Injectable({
     providedIn: 'root',
@@ -41,7 +38,7 @@ export class RecordService {
                         if (!myRecord) {
                             return { ...record, wr1st, wr1stUrl, wr10th, orLess: undefined };
                         }
-                        const orLess = this.getOrLessNumber(this.getDiff(wr1st, myRecord));
+                        const orLess = OrLessUtil.convertTimeToOrLess(wr1st, myRecord);
                         return { ...record, wr1st, wr1stUrl, wr10th, orLess };
                     });
                 }),
@@ -59,51 +56,5 @@ export class RecordService {
      */
     public getOrLessNumber(diff: number): number {
         return Math.ceil(diff / 1000);
-    }
-
-    /**
-     * その記録は指定された数値よりタイムが落ちているか
-     * @param baseSec
-     * @param diff
-     */
-    public isOrLess(baseSec: number, diff: number): boolean {
-        return baseSec * 1000 >= diff;
-    }
-
-    /**
-     * WRとの差分を取得する
-     * @param worldR
-     * @param myR
-     * @return number
-     */
-    public getDiff(worldR: string, myR: string): number {
-        const wrms = this._convertMilliSecond(this._getDivideTime(worldR));
-        const mrms = this._convertMilliSecond(this._getDivideTime(myR));
-        // 基本的に自分の記録がWRを超すことは無いため、一旦自分基準の差分で良い。
-        return mrms - wrms;
-    }
-
-    /**
-     * TAのタイムを分、秒、ミリ秒に分割する
-     * @param r
-     * @private
-     */
-    private _getDivideTime(r: string): TaData {
-        return {
-            mm: Number(r.slice(0, 1)),
-            ss: Number(r.slice(2, 4)),
-            sss: Number(r.slice(5, 8)),
-        };
-    }
-
-    /**
-     * TAのタイムをミリ秒に変換する
-     * @param mm
-     * @param ss
-     * @param sss
-     * @private
-     */
-    private _convertMilliSecond({ mm, ss, sss }: TaData): number {
-        return ((mm * 60) + ss) * 1000 + sss;
     }
 }
